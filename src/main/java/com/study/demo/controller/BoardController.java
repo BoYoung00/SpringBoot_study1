54982b27499cf003fd7ee6ad3dc5c91c1c9477cb
BoYoung00 <qhdud20@naver.com>
2023-11-20 17:42:09 +0900

    23/11/20 게시글 리스트, 뷰, 수정, 삭제 공부

--- src/main/java/com/study/demo/controller/BoardController.java
+++ src/main/java/com/study/demo/controller/BoardController.java
@@ -4,7 +4,9 @@ import com.study.demo.entity.Board;
 import com.study.demo.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,7 +27,42 @@ public class BoardController {
 
 //        System.out.println("제목 : " + board.getTitle());
 //        System.out.println("내용 : " + board.getContent());
-        return "";
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+        model.addAttribute("list", boardService.boardList());
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view") // view?id=1
+    public String boardView(Model model, int id) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(int id) {
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") int id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") int id, Board board) {
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
     }
 
 //    @GetMapping("/")
--- src/main/java/com/study/demo/service/BoardService.java
+++ src/main/java/com/study/demo/service/BoardService.java
@@ -5,12 +5,30 @@ import com.study.demo.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
+    // 글 작성 처리
     public void write(Board board) {
         boardRepository.save(board); // DB에 값 넣기
+    }
+
+    // 게시글 리스트
+    public List<Board> boardList() {
+        return boardRepository.findAll();
+    }
+
+    // 특정 게시글 불러오기 (뷰)
+    public Board boardView(int id) {
+        return boardRepository.findById(id).get();
+    }
+
+    // 게시글 삭제
+    public void boardDelete(int id) {
+        boardRepository.deleteById(id);
     }
 }
